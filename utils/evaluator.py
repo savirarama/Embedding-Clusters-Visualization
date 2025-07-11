e7192237b04c206055f9ca7bfe8b725db47b3215
savirarama <sv.vira@gmail.com>
2025-07-11 16:58:09 +0900

    add compute_mrr()

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -55,6 +55,28 @@ def compute_average_precision(recommended, actual):
     ap = ap / len(actual) if actual else 0.0
     return ap
 
+def compute_mrr(entire_list, target_list):
+    if not target_files:
+        return 0.0
+
+    reciprocal_ranks = []
+    for target_file in target_files:
+        found_rank = 0
+        for item in recommended_list:
+            if item["file"] == target_file:
+                found_rank = item["rank"]  # Use the pre-calculated rank
+                break
+        
+        if found_rank > 0:
+            reciprocal_ranks.append(1 / found_rank)
+        else:
+            reciprocal_ranks.append(0) # If not found, reciprocal rank is 0
+
+    if not reciprocal_ranks:
+        return 0.0
+        
+    return sum(reciprocal_ranks) / len(reciprocal_ranks)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find similar commits based on embeddings.')
     # parser.add_argument('--input-path', type=str, required=True,
@@ -80,6 +102,8 @@ if __name__ == '__main__':
 
     precisions, recalls, f1s = [], [], []
     aps = []
+    commit_mrrs = []
+    file_mrrs = []
     for entry in data:
         query_commits = entry.get('induceCommitHashList')
         for query_commit in query_commits:
@@ -88,10 +112,14 @@ if __name__ == '__main__':
             actual_files = commit_to_target[query_commit]
             precision, recall, f1 = compute_metrics(recommended_files[:args.n], actual_files)
             mean_ap = compute_average_precision(recommended_files, actual_files)
+            mrr = compute_mrr(recommended_files_info, actual_files)
             precisions.append(precision)
             recalls.append(recall)
             f1s.append(f1)
             aps.append(mean_ap)
+            file_mrrs.append(mrr)
+            
+        
         #print(f"QueryCommit: {query_commit}\n  Precision: {precision:.3f}, Recall: {recall:.3f}, F1: {f1:.3f}, AP: {ap:.3f}")
 
 
@@ -100,17 +128,20 @@ if __name__ == '__main__':
         avg_recall = sum(recalls) / len(recalls)
         avg_f1 = sum(f1s) / len(f1s)
         map_score = sum(aps) / len(aps)
+        avg_mrr = sum(file_mrrs) / len(file_mrrs)
         print(f"\nAverage Precision: {avg_precision:.3f}")
         print(f"Average Recall: {avg_recall:.3f}")
         print(f"Average F1: {avg_f1:.3f}")
         print(f"Mean Average Precision (MAP): {map_score:.3f}")
+        print(f"Mean Reciprocal Rank (MRR): {avg_mrr:.3f}")
 
         eval_result = {
             "k": args.n,
             "Average Precision": round(avg_precision, 3),
             "Average Recall": round(avg_recall, 3),
             "Average F1": round(avg_f1, 3),
-            "Mean Average Precision (MAP)": round(map_score, 3)
+            "Mean Average Precision (MAP)": round(map_score, 3),
+            "Mean Reciprocal Rank (MRR)": round(avg_mrr, 3)
         }
 
 
